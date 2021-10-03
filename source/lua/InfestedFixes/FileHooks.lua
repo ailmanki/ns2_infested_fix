56b3469d862c4649657e16fdd248342c26f82b32
diego <1570625+ailmanki@users.noreply.github.com>
2021-10-03 15:05:16 +0200

    removed preview pics, shine supports this now

--- source/lua/InfestedFixes/FileHooks.lua
+++ source/lua/InfestedFixes/FileHooks.lua
@@ -4,7 +4,7 @@ do
 	if nil ~= ModLoader.GetModInfo("infestedMarines") then
 		ModLoader.SetupFileHook("lua/IMGUIGameEnd.lua", "lua/InfestedFixes/IMGUIGameEnd.lua", "replace" )
 		ModLoader.SetupFileHook("lua/IMScoringMixin.lua", "lua/InfestedFixes/IMScoringMixin.lua", "post")
-				ModLoader.SetupFileHook("lua/IMGUIMarineHUD.lua", "lua/InfestedFixes/IMGUIMarineHUD.lua", "replace")
+		ModLoader.SetupFileHook("lua/IMGUIMarineHUD.lua", "lua/InfestedFixes/IMGUIMarineHUD.lua", "replace")
 		ModLoader.SetupFileHook("lua/IMNS2Gamerules.lua", "lua/InfestedFixes/IMNS2Gamerules.lua", "replace")
 	end
 end
