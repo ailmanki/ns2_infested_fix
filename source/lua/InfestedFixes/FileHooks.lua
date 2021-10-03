cefb94b2ca78433597452ac955a724b078c4b1ab
diego <1570625+ailmanki@users.noreply.github.com>
2021-10-03 12:49:46 +0200

    add vanilla ServerStats to fix errors with one team only

--- source/lua/InfestedFixes/FileHooks.lua
+++ source/lua/InfestedFixes/FileHooks.lua
@@ -1,2 +1,14 @@
-ModLoader.SetupFileHook("lua/IMGUIGameEnd.lua", "lua/InfestedFixes/IMGUIGameEnd.lua", "replace" )
-ModLoader.SetupFileHook("lua/IMScoringMixin.lua", "lua/InfestedFixes/IMScoringMixin.lua", "post")
+
+do
+	-- Only load if Infested Mod is loaded
+	if nil ~= ModLoader.GetModInfo("infestedMarines") then
+		ModLoader.SetupFileHook("lua/IMGUIGameEnd.lua", "lua/InfestedFixes/IMGUIGameEnd.lua", "replace" )
+		ModLoader.SetupFileHook("lua/IMScoringMixin.lua", "lua/InfestedFixes/IMScoringMixin.lua", "post")
+		
+		ModLoader.SetupFileHook("lua/ServerStats.lua", "lua/InfestedFixes/ServerStats.lua", "replace")
+		
+		Log("[MOD] Infested Fixes applied !")
+	else
+		Log("[MOD] Infested Fixes not loaded !")
+	end
+end
