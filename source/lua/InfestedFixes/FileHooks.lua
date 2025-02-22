d22885e4fd16c23eafb6951737a03a275bda8acd
ailmanki <1570625+ailmanki@users.noreply.github.com>
2025-02-22 12:36:41 +0100

    removed deathmessages, increased harvester health and armor

--- source/lua/InfestedFixes/FileHooks.lua
+++ source/lua/InfestedFixes/FileHooks.lua
@@ -1,8 +1,7 @@
-
--- Only load if Infested Mod is loaded
-if nil ~= ModLoader.GetModInfo("infestedMarines") then
-	ModLoader.SetupFileHook("lua/IMGUIGameEnd.lua", "lua/InfestedFixes/IMGUIGameEnd.lua", "replace" )
-	ModLoader.SetupFileHook("lua/IMScoringMixin.lua", "lua/InfestedFixes/IMScoringMixin.lua", "post")
-	ModLoader.SetupFileHook("lua/IMGUIMarineHUD.lua", "lua/InfestedFixes/IMGUIMarineHUD.lua", "replace")
-	ModLoader.SetupFileHook("lua/IMNS2Gamerules.lua", "lua/InfestedFixes/IMNS2Gamerules.lua", "replace")
-end
+ModLoader.SetupFileHook("lua/IMGUIGameEnd.lua", "lua/InfestedFixes/IMGUIGameEnd.lua", "replace" )
+ModLoader.SetupFileHook("lua/IMScoringMixin.lua", "lua/InfestedFixes/IMScoringMixin.lua", "post")
+ModLoader.SetupFileHook("lua/IMGUIMarineHUD.lua", "lua/InfestedFixes/IMGUIMarineHUD.lua", "replace")
+ModLoader.SetupFileHook("lua/IMNS2Gamerules.lua", "lua/InfestedFixes/IMNS2Gamerules.lua", "replace")
+ModLoader.SetupFileHook("lua/IMDeathMessage_Client.lua", "lua/InfestedFixes/IMDeathMessage_Client.lua", "replace")
+ModLoader.SetupFileHook("lua/IMBalanceHealth.lua", "lua/InfestedFixes/IMBalanceHealth.lua", "replace")
+ModLoader.SetupFileHook( "lua/TeamDeathMessageMixing.lua", "lua/IMTeamDeathMessageMixing.lua", "replace" )
---
+++ source/lua/InfestedFixes/IMBalanceHealth.lua
@@ -0,0 +1,11 @@
+-- ======= Copyright (c) 2003-2010, Unknown Worlds Entertainment, Inc. All rights reserved. =======
+--
+-- lua\IMBalanceHealth.lua
+--
+--    Created by:   Trevor Harris (trevor@naturalselection2.com)
+--
+-- ========= For more information, visit us at http://www.unknownworlds.com =====================
+
+kExtractorHealth = 3400
+kExtractorArmor = 1550
+kBuildPointValue = 0
